45592031d363383281f84fa82117ab2c0b0a4587
Jens Tönsing <jt@wiberry.de>
2024-02-20 17:40:19 +0100

    Status LED

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,17 @@
 
 const int PIN_PUL = 12;
 const int PIN_DIR = 6;
+const int PIN_ENA = A1;
 const int PIN_POT = A6;
+const int PIN_STATUS_MOTOR = 2;
 
 int HAL_SENSOR_STOP_VALUE = 530;
 const int HAL_SENSOR_FULL_THROTTLE_VALUE = 860;
 
 const int max_speed = 12000;
-const int acceleration = 2000;
+const int acceleration = 20000;
+// const int deceleration = 20000;
+
 int target_speed = 0;
 int last_target_speed = 0;
 boolean isPowerOff = true;
@@ -24,29 +28,34 @@ void MotorOff();
 void MotorOn();
 
 void setup() {
+
   Serial.begin(9600);
 
   cli(); // disable interrupts during setup 
+
+  pinMode(PIN_STATUS_MOTOR, OUTPUT);
+
   setupTimer1();
   setupStepper();
   setupStopValue();
+  MotorOff();
   sei(); // re-enable interrupts
 
 }
 
 void loop() {
   
-  if (last_target_speed != target_speed) {
-    last_target_speed = target_speed;
+  // Serial.println(stepper.isSpinning());
+  if ((target_speed == 0) and (!stepper.isSpinning())) {
+    MotorOff();
+  } else if ((target_speed > 0) and (!stepper.isSpinning())) {
+    MotorOn();
+  }
 
-    if (target_speed <= 0) {
-      MotorOff();
-    } else {
-      if (isPowerOff) {
-        MotorOn();
-      }
-      stepper.spin(target_speed * -1);    
-    }
+  if (last_target_speed != target_speed) {
+    Serial.println(target_speed);    
+    last_target_speed = target_speed;
+    stepper.spin(target_speed * -1);    
   }
 
   stepper.loop();
@@ -54,15 +63,21 @@ void loop() {
 }
 
 void MotorOff() {
-  stepper.powerOff();
-  isPowerOff = true;
-  Serial.println("Power off!");
+  if (!isPowerOff) {
+    stepper.powerOff();
+    digitalWrite(PIN_STATUS_MOTOR, LOW);
+    isPowerOff = true;
+    Serial.println("Power off!");
+  }
 }
 
 void MotorOn() {
-  stepper.powerOn();
-  isPowerOff = false;
-  Serial.println("Power on!");
+  if (isPowerOff) {
+    stepper.powerOn();
+    digitalWrite(PIN_STATUS_MOTOR, HIGH);
+    isPowerOff = false;
+    Serial.println("Power on!");
+  }
 }
 
 void setupStopValue() {  
@@ -77,8 +92,8 @@ void setupTimer1() {
   TCCR1B = 0;
   TCNT1 = 0;
 
-  // 2 Hz (16000000/((31249+1)*256))
-  OCR1A = 31249;
+  // 4 Hz (16000000/((15624+1)*256))
+  OCR1A = 15624;
   // CTC
   TCCR1B |= (1 << WGM12);
   // Prescaler 256
@@ -90,7 +105,8 @@ void setupTimer1() {
 
 void setupStepper() {
   stepper.begin(PIN_PUL, PIN_DIR);
-  stepper.setAcceleration(acceleration);    
+  stepper.setAcceleration(acceleration);
+  stepper.setEnablePin(PIN_ENA);
 }
 
 // Timer 1 interrupt service routine (ISR)
@@ -102,7 +118,7 @@ ISR(TIMER1_COMPA_vect)
   if (val < HAL_SENSOR_STOP_VALUE) {
     target_speed = 0;
   } else if (val > HAL_SENSOR_FULL_THROTTLE_VALUE) {
-    target_speed = max_speed;
+    target_speed = max_speed;    
   } else {
     target_speed = map(val, HAL_SENSOR_FULL_THROTTLE_VALUE, HAL_SENSOR_STOP_VALUE, max_speed, 0);
   }
